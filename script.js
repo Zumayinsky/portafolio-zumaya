dad8ad0b196fd2ee3cdab994d81b78bda73143c7
Zumayinsky <jrcarlos.20@gmail.com>
2026-06-25 22:55:22 -0600

    primer buen commit

---
+++ script.js
@@ -0,0 +1,92 @@
+{
+
+
+
+
+
+
+
+  "nombre": "Juan Carlos Zumaya Ramirez",
+
+
+
+
+
+
+
+  "titulos": [
+
+
+
+
+
+
+
+    "INGENIERO EN SISTEMAS",
+
+
+
+
+
+
+
+    "DESARROLLADOR DE SOFTWARE",
+
+
+
+
+
+
+
+    "ANALISTA DE DATOS",
+
+
+
+
+
+
+
+    "ELECTRÓNICA DIGITAL"
+
+
+
+
+
+
+
+  ],
+
+
+
+  "etiqueta": "FREELANCER EXPERTO",
+
+
+
+  "habilidades": [
+
+
+
+
+    { "nombre": "Proyectos & Experiencia", "icono": "fas fa-briefcase" },
+
+
+
+    { "nombre": "Habilidades Técnicas", "icono": "fas fa-tools" },
+
+
+
+    { "nombre": "Inglés Nivel C1", "icono": "fas fa-language" }
+
+
+  ],
+
+
+
+
+  "whatsapp": "524622647020",
+
+
+
+  "ubicacion": "Irapuato, Guanajuato, México"
+
+}
